4679a81d2e6ec8bbb4bcf658055addb33e8f927d
avendanomf <mfavendano-bermudez@outlook.com>
2024-04-29 23:57:23 -0500

    Ajustes

--- WebAppControlCitas/src/environments/environment.prod.ts
+++ WebAppControlCitas/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  URL_API: 'http://localhost:8089/api/',
+  URL_API: 'http://149.50.136.71:8089/api/',
   URL_BUCKET: 'https://bucket.colgravas.com/'
 }
